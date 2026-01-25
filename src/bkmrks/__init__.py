c898c8c1ae6816113fca6debd63478d7f9ccc4ac
Cesar Bouli <contato@bouli.com.br>
2026-01-25 01:28:54 +0100

    Bump version: 0.1.1 → 0.1.2

--- src/bkmrks/__init__.py
+++ src/bkmrks/__init__.py
@@ -6,7 +6,7 @@ from bkmrks import bkmrks, md, presenter
 
 def main():
     parser = argparse.ArgumentParser(prog="bkmrks")
-    parser.add_argument("--version", action="version", version="%(prog)s v0.1.1")
+    parser.add_argument("--version", action="version", version="%(prog)s v0.1.2")
 
     subparsers = parser.add_subparsers(dest="command", help="Available commands")
     render_parser = subparsers.add_parser(
