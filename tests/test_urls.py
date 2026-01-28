536acbf1cd346104f51e970816f23490cab6c935
Cesar Bouli <contato@bouli.com.br>
2026-01-28 17:27:26 +0100

    tests(bkmrks.urls): Add tests to `get_name_by_url`

--- tests/test_urls.py
+++ tests/test_urls.py
@@ -26,4 +26,24 @@ def test_ensure_domain():
 
     with pytest.raises(ValueError):
         urls.ensure_domain(url="/path",domain="not-a-domain.com")
+
+def test_get_url_icon():
     return
+
+def test_get_default_img():
+    with pytest.raises(TypeError):
+        urls.get_default_img()
+    assert "https://ui-avatars.com/api/?name=testing" == urls.get_default_img(text="testing")
+    assert "https://ui-avatars.com/api/?name=test%20ing" == urls.get_default_img(text="test ing")
+
+
+def test_get_name_by_url():
+    with pytest.raises(TypeError):
+        urls.get_name_by_url()
+    with pytest.raises(ValueError):
+        assert "testing" == urls.get_name_by_url(url="testing.com.br")
+    assert "testing" == urls.get_name_by_url(url="http://www.testing.com.br")
+    assert "testing" == urls.get_name_by_url(url="http://testing.com.br")
+    assert "testing" == urls.get_name_by_url(url="http://testing.com")
+    assert "google_gservice" == urls.get_name_by_url(url="http://gservice.google.com")
+    assert "google_gservice" == urls.get_name_by_url(url="http://subdomain.gservice.google.com")
